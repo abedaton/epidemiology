985766dd95c0af3bc817a004162921767e1c497e
Cdemuele <32980367+Cdemuele@users.noreply.github.com>
2019-11-27 21:45:06 +0100

    Version un peu moins pire du graphe

--- nodeGrid.py
+++ nodeGrid.py
@@ -3,56 +3,36 @@ import networkx as nx
 from random import randint
 import numpy as np
 from scipy.integrate import odeint
+from all import *
 
 class NodeGrid(object):
     """docstring for NodeGrid."""
 
-    def __init__(self, n=1001, color_s='gray',color_i='red',scenario='global',t=10):
-    
-    
-        self.n=n
-        self.sizeX = 10
-        self.sizeY = 5
-        print(self.sizeX,self.sizeY)
+    def __init__(self, scenario='global',N=101,I0=1,R0=0,S0=100,t=1000,param={"beta" : 0.2, "gamma" : 1./10},sim_type="SIR"):
+        self.sizeX = int((N**0.5)+1)
+        self.sizeY = int(N/int(N**0.5))
         
         self.scenario=scenario
-        self.s_Color = color_s #susceptible
-        self.i_Color = color_i #infecté
-        self.run_sim()
+         
+        #self.b_Color = 'grey'
+        #self.s_Color = 'grey'
+        #self.e_Color = 'blue'
+        #self.i_Color = 'red'
+        #self.r_Color = 'green'
+        
         self.prepareGraph()
         
-        for i in range (t):
-            self.plot()
-            self.startInfection()
-    
-    def run_sim(self):
-        I0=1
-        R0 = 0
-        N = 1001
-        # Tous les autres sont donc susceptible 
-        S0 = N - I0 - R0
-        # beta = virulance (contact rate) et gamma = "recovery rate" (/jours).
-        beta, gamma = 0.2, 1./10 
+        Ep = Epidemy(N,I0,R0,S0,t,param)
+        self.S, self.I, self.R = Ep.solve(sim_type)
+        #pour l'instant, ne pas choisir autre chose que sir
 
-        t = np.linspace(0, 1000, 1000)
+        #print(len(self.S),len(self.I),len(self.R))
+        
+        #print(self.S[i],self.I[t],self.R[t])
 
-        # Equadif de SIR
-        def deriv(y, t, N, beta, gamma):
-            S, I, R = y
-            dSdt = -beta * S * I / N
-            dIdt = beta * S * I / N - gamma * I
-            dRdt = gamma * I
-            return dSdt, dIdt, dRdt
-
-        # vecteur initial
-        y0 = S0, I0, R0
-
-        # On résoud l'equadiff
-        ret = odeint(deriv, y0, t, args=(N, beta, gamma))
-        S, I, R = ret.T
-        self.S=S
-        self.I=I
-        self.R=R
+        for i in range (t-1):
+        	#print(self.S[i],self.I[i],self.R[i])
+            self.plot(i)
 
     def prepareGraph(self):
         self.G = nx.grid_2d_graph(self.sizeX, self.sizeY)
@@ -63,56 +43,62 @@ class NodeGrid(object):
         # sur chaque noeud
         self.labels = dict( ((i, j), " ") for i, j in self.G.nodes() )
 
-        self.colors = [self.s_Color for i in range(len(self.G.nodes()))]
+        self.colors = ['grey' for i in range(len(self.G.nodes()))]
 
     def changeNodeAtColor(self, i, j, color):
         self.colors[i*self.sizeY + j] = color
 
-    def startInfection(self, I=1):
-        to_change=[]
-        for i in range (self.sizeY):
-            for j in range (self.sizeY):
+    def plot(self,t):
+    	#to_change=[]
+        #delta_e=
+        delta_i=int(self.I[t])-int(self.R[t-1])
+        delta_r=int(self.I[t])-int(self.R[t-1])
+        
+        #Ajout des nouveaux infectés
+        if delta_i>0:
+            for new_victim in range (delta_i):
+            	if 'grey' in self.colors:
+                    _new_x=randint(0,self.sizeX)
+                    _new_y=randint(0,self.sizeY)
                 
-                if self.colors[i*self.sizeY + j] == self.s_Color and self.scenario=='local':
-                    if i>0 and randint(0,10)==1:
-                        i_victim=i-1
-                        j_victim=j
-                        
-                        self.changeNodeAtColor(i_victim,j_victim, 'red')
-                        #to_change.append((i*j,i_victim*j_victim))
-                        
-                    if j>0 and randint(0,10)==1:
-                        i_victim=i
-                        j_victim=j-1
-                        
-                        self.changeNodeAtColor(i_victim,j_victim, 'red')
-                        #to_change.append((i*j,i_victim*j_victim))
-                        
-                    if i<self.sizeY and randint(0,10)==1:
-                        i_victim=i+1
-                        j_victim=j
-                        
-                        self.changeNodeAtColor(i_victim,j_victim, 'red')
-                        #to_change.append((i*j,i_victim*j_victim))
-                          
-                    if j<self.sizeX and randint(0,10)==1:
-                        i_victim=i
-                        j_victim=j+1
+                    col='red'
 
-                        self.changeNodeAtColor(i_victim,j_victim, 'red')
-                        #to_change.append((i*j,i_victim*j_victim))
-         
-                if self.colors[i*self.sizeY + j] == self.s_Color and self.scenario=='global':
-                    if randint(0,10)==1:
-                        i_victim=randint(0,self.sizeY)
-                        j_victim=randint(0,self.sizeX)
-                
-                        self.changeNodeAtColor(i_victim,j_victim, 'red')
-                        #to_change.append((i*j,i_victim*j_victim))
-                    
-        self.G.add_edges_from(to_change)
+                    while col!='grey':
+                        _new_x=randint(0,self.sizeX)
+                        _new_y=randint(0,self.sizeY)
+                        try:
+                            col=self.colors[_new_x*self.sizeY + _new_y]
 
-    def plot(self):
+                        except:
+                            col='red'
+
+                    #to_change.append((i*j,i_victim*j_victim))
+                    self.changeNodeAtColor(_new_x,_new_y,'red')
+
+
+        #Ajout des nouveaux recovered
+        if delta_r>0 :
+            for new_victim in range (delta_r):
+            	if 'red' in self.colors:
+                    _new_x=randint(0,self.sizeX)
+                    _new_y=randint(0,self.sizeY)
+                   
+                    col='green'
+
+                    while  col!='red':
+                        _new_x=randint(0,self.sizeX)
+                        _new_y=randint(0,self.sizeY)
+                        try:
+                        	col=self.colors[_new_x*self.sizeY + _new_y]
+                        except:
+                        	col='green'
+
+ 
+                    #to_change.append((i*j,i_victim*j_victim))
+                    self.changeNodeAtColor(_new_x,_new_y,'green')
+
+        #self.G.add_edges_from(to_change)
+
         nx.draw_networkx(self.G, pos=self.pos, labels=self.labels, node_color=self.colors )
         plt.show()
 
