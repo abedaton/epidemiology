c7b2ba6cb2731070945782acfe3053748e27c7d9
Nathan <nmarotte@ulb.ac.be>
2019-11-28 14:17:27 +0100

    PixelGrid animated

--- nodeGrid.py
+++ nodeGrid.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import networkx as nx
-from random import randint
+import random as rd
 import numpy as np
 from scipy.integrate import odeint
 from all import *
@@ -9,31 +10,103 @@ from all import *
 #utiliser les modeles ?
 #changer x et y en p
 
+class PixelGrid(object):
+    """docstring for PixelGrid."""
+
+    def __init__(self, sizeX, sizeY, base = 0):
+        super(PixelGrid, self).__init__()
+        self.X = sizeX
+        self.Y = sizeY
+        self.pixels = np.zeros((sizeY, sizeX))
+        self.infected = set()
+        self.cured = set()
+
+    def __getitem__(self, key):
+        if isinstance(key, int): #PixelGrid[0]
+            return self.pixels[key]
+        else: #PixelGrid[(0,0)]
+            return self.pixels[key[0]][key[1]]
+
+    def startInfection(self, I0=1):
+        for patientsZero in range(I0):
+            i = rd.randint(0,self.Y-1)
+            j = rd.randint(0,self.X-1)
+            self.infect((i,j))
+        self.createHeatmap()
+
+    def infect(self, cell):
+        if cell not in self.cured:
+            self.pixels[cell] = 1
+            self.infected.add(cell)
+
+    def cure(self, cell):
+        self.pixels[cell] = 0
+        self.infected.remove(cell)
+        self.cured.add(cell)
+
+    def stepInfection(self, infectNeighbourProb=1, cureProb=0.1):
+        infected = list(self.infected)
+        for i,j in infected:
+            for cell in self.neighbors(i,j):
+                if luckCheck(infectNeighbourProb):
+                    self.infect(cell)
+            if luckCheck(cureProb):
+                self.cure((i,j))
+
+    def createHeatmap(self, cmap='hot'):
+        self.figure = plt.gcf()
+        self.image = plt.imshow(self.pixels, cmap=cmap)
+
+    def plot(self):
+        self.makeHeatmap()
+        plt.show()
+
+    def refreshHeatmap(self, frame):
+        self.stepInfection()
+        self.image.set_data(self.pixels)
+
+
+    def neighbors(self, icell,j):
+        res = []
+        for i in (icell-1, icell, icell+1):
+            if 0 <= i <= self.Y-1: #Si i n'est pas out of bound
+                if j > 0: #Si j n'est pas la première case
+                    res.append((i, j-1))
+                if i != icell: #On est pas voisin avec soi-même
+                    res.append((i, j))
+                if j < self.X-1: #Si j n'est pas la dernière case
+                    res.append((i, j+1))
+        return res
+
+def luckCheck(lowerThan):
+    return rd.uniform(0,1) <= lowerThan
+
+
 class NodeGrid(object):
     """docstring for NodeGrid."""
 
     def __init__(self, scenario='global',N=101,I0=1,R0=0,S0=100,t=1000,param={"beta" : 0.2, "gamma" : 1./10},sim_type="SIR"):
         self.sizeX = int((N**0.5)+1)
         self.sizeY = int(N/int(N**0.5))
-        
+
         self.N=N
-        
+
         self.scenario=scenario
-         
+
         #self.b_Color = 'grey'
         #self.s_Color = 'yellow'
         #self.e_Color = 'blue'
         #self.i_Color = 'red'
         #self.r_Color = 'green'
-        
+
         self.prepareGraph()
-        
+
         Ep = Epidemy(N,I0,R0,S0,t,param)
         self.S, self.I, self.R = Ep.solve(sim_type)
         #pour l'instant, ne pas choisir autre chose que sir
 
         #print(len(self.S),len(self.I),len(self.R))
-        
+
         #print(self.S[i],self.I[t],self.R[t])
 
         pas=5
@@ -63,18 +136,18 @@ class NodeGrid(object):
         #delta_s=int(((self.I[t]-self.R[t-1])/100)*self.N)
         delta_i=int(((self.I[t]-self.I[t-dt])/100)*self.N)
         delta_r=int(((self.R[t]-self.R[t-dt])/100)*self.N)
-        
-        
+
+
         #print((int(self.S[t-dt])/100)*self.N, (int(self.I[t])/100)*self.N, (int(self.R[t])/100)*self.N)
         #print((int(self.S[t])/100)*self.N, (int(self.I[t])/100)*self.N, (int(self.R[t])/100)*self.N)
-        
+
         #Ajout des nouveaux infectés
         if delta_i>0:
             for new_victim in range (delta_i):
                 if 'yellow' in self.colors:
                     _new_x=randint(0,self.sizeX)
                     _new_y=randint(0,self.sizeY)
-                
+
                     col='red'
 
                     while col!='yellow':
@@ -94,14 +167,14 @@ class NodeGrid(object):
         # suceptible jaune
         # infecté rouge
         # gueri vert
-        
+
         #Ajout des nouveaux recovered
         if delta_r>0 :
             for new_victim in range (delta_r):
                 if 'red' in self.colors:
                     _new_x=randint(0,self.sizeX)
                     _new_y=randint(0,self.sizeY)
-                   
+
                     col='green'
 
                     while  col!='red':
@@ -112,16 +185,22 @@ class NodeGrid(object):
                         except:
                         	col='green'
 
- 
+
                     #to_change.append((i*j,i_victim*j_victim))
                     self.changeNodeAtColor(_new_x,_new_y,'green')
                 else:
                     print("erreur 404, pas d'infectés")
         #self.G.add_edges_from(to_change)
-        
+
     def plot(self):
         nx.draw_networkx(self.G, pos=self.pos, labels=self.labels, node_color=self.colors )
         plt.show()
 
 if __name__ == '__main__':
-    NodeGrid()
+    #NodeGrid()
+    test = PixelGrid(10,30)
+    #test.plot()
+    test.startInfection(10)
+    #test.plot()
+    ani = animation.FuncAnimation(test.figure, test.refreshHeatmap, frames=100, interval=50)
+    plt.show()
