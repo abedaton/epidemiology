0dd5d3bdc08911f5d80798d512d24912aa4ae6b7
pdefraene <pdefraene@Bebemutant.pool.ulb.ac.be>
2019-12-12 14:17:55 +0100

    need to add classe now

--- GUI_pyqt.py
+++ GUI_pyqt.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton, QSlider,QHBoxLayout,QVBoxLayout,QTabWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton, QSlider,QHBoxLayout,QVBoxLayout,QTabWidget,QSpinBox, QLabel
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 
@@ -13,15 +13,50 @@ import random
 
 class App(QWidget):
 
-    def __init__(self):
+    def __init__(self,model):
         super().__init__()
         self.left = 10
         self.top = 10
         self.title = "Simulation d'épidémie"
         self.width = 640
         self.height = 400
+        self.model = model
+
+        self.box = []
 
         self.layout = QVBoxLayout(self)
+        self.layout_param_init = QHBoxLayout(self)
+        self.layout_proba = QHBoxLayout(self)
+        self.layout_pop = QVBoxLayout(self)
+        self.layout_but = QVBoxLayout(self)
+
+        self.population_name = QLabel()
+        self.population_name.setText("Population totale")
+
+        # out of tab
+        self.population = QSpinBox()
+        self.population.setRange(0,1000000)
+        self.population.setValue(1000)
+        self.population.setSingleStep(1000)
+
+        self.button = QPushButton('Lancer simulation', self)
+        self.button.setToolTip('créer les nouveaux graphiques aves les nouvelles valeurs')
+        self.button.clicked.connect(self.plop)
+
+        self.layout_but.addWidget(self.button)
+
+
+        self.layout_pop.addWidget(self.population_name)
+        self.layout_pop.addWidget(self.population)
+
+        self.layout_param_init.addLayout(self.layout_pop)
+        self.layout_param_init.addLayout(self.layout_but)
+
+        self.layout.addLayout(self.layout_param_init)
+        
+        self.layout.addLayout(self.layout_proba)
+        
+
         # Initialize tab screen
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
@@ -34,45 +69,31 @@ class App(QWidget):
         
         # Create first tab
         self.tab1.layout = QVBoxLayout(self)
-        self.button = QPushButton('Lancer simulation', self)
-        self.button.setToolTip('créer les nouveaux graphiques aves les nouvelles valeurs')
-        self.button.clicked.connect(self.plop)
+        
 
         self.graph = PlotCanvas(self, width=5, height=4)
 
-        self.slider = QSlider(Qt.Horizontal, self)
-        self.slider.setTickPosition(QSlider.TicksBothSides)
-        self.slider.setTickInterval(10)
-        self.slider.setSingleStep(1)
+        #self.slider = QSlider(Qt.Horizontal, self)
+        #self.slider.setTickPosition(QSlider.TicksBothSides)
+        #self.slider.setTickInterval(10)
+        #self.slider.setSingleStep(1)
 
-        self.tab1.layout.addWidget(self.slider)
+        #self.tab1.layout.addWidget(self.slider)
         self.tab1.layout.addWidget(self.graph)
-        self.tab1.layout.addWidget(self.button)
+
+        
 
         self.tab1.setLayout(self.tab1.layout)
         
         # Add tabs to widget
+        
         self.layout.addWidget(self.tabs)
+        
         self.setLayout(self.layout)
 
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-
-
-         
-
-         
-
-        # hbox.addWidget(self.button)
-        # hbox.addWidget(self.slider)
-
-        # vbox = QVBoxLayout()
-        # vbox.addStretch(1)
-        # vbox.addLayout(hbox)
-
-        
-        # self.setLayout(vbox)
         self.show()
     def plop(self):
         print("hello")
@@ -103,6 +124,6 @@ class PlotCanvas(FigureCanvas):
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
+    app = QApplication(sys.argv) 
+    ex = App("SRI") # prend la classe à créer en paramètre
     sys.exit(app.exec_())
