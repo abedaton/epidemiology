77277b7db553fd02e6b73923012c8468e681efce
Cdemuele <32980367+Cdemuele@users.noreply.github.com>
2019-12-13 00:13:55 +0100

    ajout plot

--- modele_graph.py
+++ modele_graph.py
@@ -1,10 +1,14 @@
 import random
-
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import time
+import matplotlib.animation as animation
 
 
 class modele(object):
 	def __init__(self,H=100,L=100,n=6,I0=37,mwm=1,mmw=1,mww=0,mmm=0,bw=2,aw=1,R0w=2,bm=7.2,am=4,R0m=1.8,gl=False,loc=True):
-		self.mat=[['S' for i in range (L)] for j in range (H)]
+		self.mat=[[0 for i in range (L)] for j in range (H)]
 
 		self.n=n
 		self.H=H
@@ -28,11 +32,13 @@ class modele(object):
 		j0=int(L/2)-int(lc/2)
 
 		for i in range (I0):
-			self.mat[i0][j0]='W'
+			self.mat[i0][j0]=1
 			j0=j0+1
 			if j0>int(H/2)+int(lc/2):
 				j0=int(H/2)-int(lc/2)
 				i0=i0+1
+
+		self.heatmap()
 
 	def gen_bool_from_a_certian_probability_out_of_100(self,prob):
 		res=False
@@ -47,22 +53,22 @@ class modele(object):
 		#Mutation
 		for i in range (len(self.mat)):
 			for j in range (len(self.mat[0])):
-				if self.mat[i][j]=='M':
+				if self.mat[i][j]==2:
 					temp=self.gen_bool_from_a_certian_probability_out_of_100(self.mmw)
 					if temp:
-						self.mat[i][j]='W'
+						self.mat[i][j]=1
 
-				if self.mat[i][j]=='W':
+				if self.mat[i][j]==1:
 					temp=self.gen_bool_from_a_certian_probability_out_of_100(self.mwm)
 					if temp:
-						self.mat[i][j]='M'
+						self.mat[i][j]=2
 
 		#Local
 		if self.loc:
 			for i in range (len(self.mat)):
 				for j in range (len(self.mat[0])):
 					#Virulent
-					if self.mat[i][j]=='M':
+					if self.mat[i][j]==2:
 						for gens in range (self.n):
 							temp=self.gen_bool_from_a_certian_probability_out_of_100(self.bm)
 							if temp:
@@ -70,31 +76,31 @@ class modele(object):
 								new_j=i
 								ite=0
 								k=1
-								while self.mat[new_i][new_j]=='M':
+								while self.mat[new_i][new_j]==2:
 									ite=ite+1
-									new_i=i+random.randint(-k,k)
-									new_j=j+random.randint(-k,k)
+									new_i=max(0,min((len(self.mat))-1,(i+random.randint(-k,k))))
+									new_j=max(0,min((len(self.mat[0]))-1,j+random.randint(-k,k)))
 									if ite==25:
 										k=k+1
 										ite=0
-								self.mat[new_i][new_j]='M'
+								self.mat[new_i][new_j]=2
 					#Wild
-					if self.mat[i][j]=='W':
+					if self.mat[i][j]==1:
 						for gens in range (self.n):
 							temp=self.gen_bool_from_a_certian_probability_out_of_100(self.bw)
 							if temp:
-								new_i=j
-								new_j=i
+								new_i=j-1
+								new_j=i-1
 								ite=0
 								k=1
-								while self.mat[new_i][new_j]=='W':
+								while self.mat[new_i][new_j]==1:
 									ite=ite+1
-									new_i=i+random.randint(-k,k)
-									new_j=j+random.randint(-k,k)
+									new_i=max(0,min((len(self.mat))-1,i+random.randint(-k,k)))
+									new_j=max(0,min((len(self.mat[0]))-1,j+random.randint(-k,k)))
 									if ite==25:
 										k=k+1
 										ite=0
-								self.mat[new_i][new_j]='W'
+								self.mat[new_i][new_j]=1
 						#temp=self.gen_bool_from_a_certian_probability_out_of_100(self.)
 						#Faire un truc avc la virulence ?
 					
@@ -104,20 +110,20 @@ class modele(object):
 			for i in range (len(self.mat)):
 				for j in range (len(self.mat[0])):
 					#Virulent
-					if self.mat[i][j]=='M':
+					if self.mat[i][j]==2:
 						temp=self.gen_bool_from_a_certian_probability_out_of_100(self.bm)
 						if temp:
 							new_i=random.randint(0,self.H-1)
 							new_j=random.randint(0,self.L-1)
-							self.mat[new_i][new_j]='M'
+							self.mat[new_i][new_j]=2
 
 					#Wild
-					if self.mat[i][j]=='W':
+					if self.mat[i][j]==1:
 						temp=self.gen_bool_from_a_certian_probability_out_of_100(self.bw)
 						if temp:
 							new_i=random.randint(0,self.H-1)
 							new_j=random.randint(0,self.L-1)
-							self.mat[new_i][new_j]='W'
+							self.mat[new_i][new_j]=1
 
 
 						#temp=self.gen_bool_from_a_certian_probability_out_of_100(self.aw)
@@ -125,17 +131,22 @@ class modele(object):
 						#Faire un truc avc la virulence ?
 
 
-	def display(self):
-		print()
-		for i in range (len(self.mat)):
-			for j in range (len(self.mat[0])):
-				print(self.mat[i][j],end='')
-			print()
 
 
-x=modele()
-x.display()
+	def heatmap(self):
+		self.fig, ax_lst = plt.subplots(1,1)
+		heatmap = ax_lst.pcolor(self.mat)
+		self.fig.canvas.draw()
+		self.fig.show()
 
-for i in range (100):
-	x.spread()
-	x.display()
+		while True:
+			self.spread()
+
+			heatmap = ax_lst.pcolor(self.mat)
+			ax_lst.draw_artist(ax_lst.patch)
+			ax_lst.draw_artist(heatmap)
+			self.fig.canvas.blit(ax_lst.bbox)
+			self.fig.canvas.flush_events()
+
+if __name__=='main':
+	x=modele()
