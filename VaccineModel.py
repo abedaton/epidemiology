446412c2f4b9c5308a6ff025acd9c2ff6d6cd744
nmarotte <nathan.marotte@ulb.ac.be>
2020-02-07 18:49:17 +0100

    Refactored

--- VaccineModel.py
+++ VaccineModel.py
@@ -1,4 +1,7 @@
 import random
+SUSCEPTIBLE = 0
+INFECTED = 1
+VACCINATED = 2
 
 def RNG(probability):
     return random.random() < probability
@@ -6,91 +9,118 @@ def RNG(probability):
 class VaccineModel(object):
     """docstring for VaccineModel."""
 
-    def __init__(self, parametres = {}, size = [30,30]):
-        self.counter = 0
+    def __init__(self, parametres = {}, size = [50,50]):
         self.X, self.Y = size
+        self.counter = 0
+
         self.parametres = parametres
-        self.currentValues = {'nbSusceptible' : self.X*self.Y, 'nbInfected' : 0, 'nbVaccinated' : 0}
-        self.representation = {'SUSCEPTIBLE' : 0, 'INFECTED' : 1, 'VACCINATED' : 2}
+
+        #Création d'un set de toutes les pos
+        self.susceptibles = set()
+        for i in range(self.X):
+            for j in range(self.Y):
+                self.susceptibles.add((i,j))
+
+        self.vaccinated = set()
+        self.infected = set()
         self.defaultParametres()
-        self.population = [[self.get('SUSCEPTIBLE') for j in range(self.X)] for i in range(self.Y)]
+
+        self.population = [[SUSCEPTIBLE for j in range(self.X)] for i in range(self.Y)]
+
+
+    def startAnimation(self):
         self.vaccinatePopulation()
+        self.startInfection()
 
+    def changeParam(self, parametres):
+        #prend un dictionnaire en parametres et change les valeurs de prob
+        self.parametres = parametres
+        self.defaultParametres()
 
-
-    def get(self, nameOfParam):
-        if nameOfParam in self.parametres:
-            return self.parametres[nameOfParam]
-        if nameOfParam in self.currentValues:
-            return self.currentValues[nameOfParam]
-        if nameOfParam in self.representation:
-            return self.representation[nameOfParam]
 
     def defaultParametres(self):
-        self.defaults = {'base' : 0, 'probVaccine' : 0.5, 'probInfect' : 1, 'maxTime' : 50}
-        for elem in self.defaults.keys():
+        defaults =\
+        {'probVaccine' : 0.5,\
+         'probInfect' : 1,\
+          'maxTime' : 50,\
+          'I0' : 1\
+        }
+        for elem in defaults.keys():
             if elem not in self.parametres:
-                self.parametres[elem] = self.defaults[elem]
+                self.parametres[elem] = defaults[elem]
 
     def vaccinatePopulation(self):
-        for i, row in enumerate(self.population):
-            for j, people in enumerate(row):
-                if RNG(self.get('probVaccine')):
-                    self.vaccinate(i,j)
+        #Nombre total de gens vacciné
+        toVaccine = self.parametres['probVaccine']*(self.X*self.Y)
+        while toVaccine > 0:
+            #On vaccine la population au hasard
+            i, j = random.randrange(0, self.Y), random.randrange(0, self.X)
+            #Si vaccin réussi, on diminue
+            if self.vaccinate((i, j)):
+                toVaccine -= 1
 
-    def vaccinate(self, i, j):
-        self.population[i][j] = self.get('VACCINATED')
-        self.currentValues['nbSusceptible'] -= 1
-        self.currentValues['nbVaccinated'] += 1
+    def vaccinate(self, indexPair):
+        i, j = indexPair
+        if indexPair in self.susceptibles: #Vaccine que les sus
+            self.susceptibles.remove(indexPair)
+            self.vaccinated.add(indexPair)
+            self.population[i][j] = VACCINATED
+            return True
+        return False
 
-    def infect(self, i, j):
-        if self.population[i][j] == self.get('SUSCEPTIBLE'):
-            self.population[i][j] = self.get('INFECTED')
-            self.currentValues['nbSusceptible'] -= 1
-            self.currentValues['nbInfected'] += 1
+    def infect(self, indexPair):
+        i, j = indexPair
+        if indexPair in self.susceptibles: #infecte que le sus
+            self.susceptibles.remove(indexPair)
+            self.infected.add(indexPair)
+            self.population[i][j] = INFECTED
+            return True
+        return False
 
-    def startInfection(self, I0 = 1):
-        while I0 > 0:
-            I0 -= 1
-            #force infection
-            self.population[random.randrange(0, self.Y)][random.randrange(0,self.X)] = self.get('INFECTED')
+    def startInfection(self):
+        susceptibles = tuple(self.susceptibles)
+        #Infect un susceptible aléatoire
+        self.infect(susceptibles[random.randrange(0,len(susceptibles))])
+
 
     def spread(self):
+        stack = []
+        for iSus, jSus in self.susceptibles:
+            for i,j in self.neighbours(iSus, jSus):
+                if (i,j) in self.infected and RNG(self.parametres['probInfect']):
+                    stack.append((iSus, jSus))
+        for futureinfected in stack:
+            self.infect(futureinfected)
         self.counter += 1
-        for iOne, row in enumerate(self.population):
-            for jOne, people in enumerate(row):
-                for i,j in self.neighbours(iOne,jOne):
-                    if self.population[i][j] == self.get('INFECTED') and RNG(self.get('probInfect')):
-                        self.infect(iOne,jOne)
-        if self.counter == self.get('maxTime')-1:
+        #Fin du spreading, on affiche les résultats
+        if self.counter == self.parametres['maxTime']-1:
             nombreSainDépart = self.X*self.Y
-            nombreVacciné = self.get('nbVaccinated')
-            nombreInfecté = self.get('nbInfected')
-            nombreSain = self.get('nbSusceptible')
-            print(nombreSain, nombreInfecté, nombreVacciné)
-            print(f"Pour une population vaccinée à {self.get('probVaccine')*100}%")
-            print(f"Nous constatons que parmis les non vaccinés, \
-            {nombreSain/(nombreSain+nombreInfecté)}% ont été infectés")
+            nombreVacciné = len(self.vaccinated)
+            nombreInfecté = len(self.infected)
+            nombreSain = len(self.susceptibles)
+            print(f"Pour une population vaccinée à {self.parametres['probVaccine']*100}%")
+            print(f"Nous constatons que parmis les non vaccinés ( {nombreSain+nombreInfecté} cases blanches au départ), \
+            seulement {100*nombreSain/(nombreSain+nombreInfecté)}% ont été épargnés du virus")
 
     def neighbours(self, i,j):
-        res = []
+        res = set()
         if 0 < i-1:
             if 0 < j-1:
-                res.append((i-1,j-1))
-            res.append((i-1,j))
+                res.add((i-1,j-1))
+            res.add((i-1,j))
             if j+1 < self.X:
-                res.append((i-1, j+1))
+                res.add((i-1, j+1))
 
         if 0 < j-1:
-            res.append((i,j-1))
-        res.append((i,j))
+            res.add((i,j-1))
+        res.add((i,j))
         if j+1 < self.X:
-            res.append((i, j+1))
+            res.add((i, j+1))
 
         if i+1 < self.Y:
             if 0 < j-1:
-                res.append((i+1,j-1))
-            res.append((i+1,j))
+                res.add((i+1,j-1))
+            res.add((i+1,j))
             if j+1 < self.X:
-                res.append((i+1, j+1))
+                res.add((i+1, j+1))
         return res
