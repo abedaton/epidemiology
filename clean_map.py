775cf677ddd97ab1abbcf1eb8863267a76692da9
Bedaton Antoine <abedaton@ulb.ac.be>
2020-02-10 14:11:02 +0100

    working? pierre

--- clean_map.py
+++ clean_map.py
@@ -1,19 +1,21 @@
 # GUI
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from matplotlib.pyplot import ion, axes, title, waitforbuttonpress, scatter, pause, ginput
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_qt5agg import FigureCanvas
+#from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton
 
 # Geocoding / Map
 import cartopy.crs as ccrs
-import pycristoforo as pyc
-from shapely.geometry import Point
-import fiona
 import cartopy.io.shapereader as shpreader
+import pycristoforo as pyc
+import fiona
+from shapely.geometry import Point
 import shapely.geometry as sgeom
 from shapely.prepared import prep
 import reverse_geocoder as rg
-
 import country_converter as coco
 
 import numpy as np
@@ -30,17 +32,17 @@ class Map(FigureCanvas):
         self.startCountry = None
         self.cc = coco.CountryConverter()
 
-        plt.ion()  # mets en mode interactif
-        self.ax = plt.axes(projection=eval(projectionType))  # dis quel type de map on veut
+        ion()  # mets en mode interactif
+        self.ax = axes(projection=eval(projectionType))  # dis quel type de map on veut
         self.ax.stock_img()  # Ajoute l'image au graph
         self.x0, self.x1, self.y0, self.y1 = self.ax.get_extent()
 
-        plt.title("Choose somewhere to start", fontsize=50)
+        title("Choose somewhere to start", fontsize=50)
         self.draw()
         #country = self.waitForStart()
 
         #self.infect(0.0000001, 1000, coco.convert(names=country, to="ISO3"))
-        #plt.waitforbuttonpress(0)
+        #waitforbuttonpress(0)
 
     def infect(self, timeInterval, num, Thecountry):
         liste = [pyc.get_shape(Thecountry)]
@@ -52,8 +54,8 @@ class Map(FigureCanvas):
                 points = self.findPoints(country)
                 x = points.x
                 y = points.y
-                plt.scatter(x, y, color="red", marker=".", transform=ccrs.Geodetic())
-                plt.pause(timeInterval)
+                scatter(x, y, color="red", marker=".", transform=ccrs.Geodetic())
+                pause(timeInterval)
 
     def findPoints(self, country):
         minx, miny, maxx, maxy = country.bounds
@@ -77,17 +79,17 @@ class Map(FigureCanvas):
                 result = rg.search((y, x))
                 country = self.cc.convert(names=result[0]["cc"], to="name_short")
                 print("Starting in", country)
-                plt.title("Starting in " + str(country), fontsize=50)
+                title("Starting in " + str(country), fontsize=50)
                 self.go = True
                 return country
             else:
                 print("Mer")
-                plt.title("Please choose a location on land !", fontsize=50)
+                title("Please choose a location on land !", fontsize=50)
 
 
     def waitForStart(self):
         while not self.go:
-            location = plt.ginput(1, timeout=0)
+            location = ginput(1, timeout=0)
             country = self.transformToCountry(location[0][0], location[0][1])
         return country
 
